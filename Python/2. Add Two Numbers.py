a92ba0e8a9ebe202f6b6c546a748dd3f888be84a
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-04-19 13:49:01 +0600

    Update 2. Add Two Numbers.py
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

--- Python/2. Add Two Numbers.py
+++ Python/2. Add Two Numbers.py
@@ -1,8 +1,3 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         cur = res = ListNode()
