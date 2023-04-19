676e7eaf02814e2ae91d0775f5e195d6e8adbbf4
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-04-19 13:43:48 +0600

    Create 2. Add Two Numbers.py
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

---
+++ Python/2. Add Two Numbers.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur = res = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            carry, value = carry // 10, carry % 10
+            cur.next = ListNode(value)
+            cur = cur.next
+        return res.next
