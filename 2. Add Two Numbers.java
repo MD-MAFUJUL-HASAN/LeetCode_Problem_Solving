6c19a43a913e5df0fdbd038ba7af9784314ed07a
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-04-25 12:57:29 +0600

    Update 2. Add Two Numbers.java
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

--- 2. Add Two Numbers.java
+++ 2. Add Two Numbers.java
@@ -1,22 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode cur = new ListNode();
-        ListNode res = cur;
+        ListNode dummy = new ListNode(0);
         int carry = 0;
+        ListNode cur = dummy;
         while (l1 != null || l2 != null || carry != 0) {
-            if (l1 != null) {
-                carry += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                carry += l2.val;
-                l2 = l2.next;
-            }
-            int value = carry % 10;
-            carry /= 10;
-            cur.next = new ListNode(value);
+            int s = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = s / 10;
+            cur.next = new ListNode(s % 10);
             cur = cur.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
-        return res.next;
+        return dummy.next;
     }
 }
