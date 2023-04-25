00b2d4ce880d43d2b2e3ae54d1392c1302fd4d5d
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-04-25 12:57:57 +0600

    Update 2. Add Two Numbers.js
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

--- 2. Add Two Numbers.js
+++ 2. Add Two Numbers.js
@@ -1,20 +1,26 @@
-var addTwoNumbers = function(l1, l2) {
-    let cur = new ListNode();
-        let res = cur;
-        let carry = 0;
-        while (l1 || l2 || carry) {
-            if (l1) {
-                carry += l1.val;
-                l1 = l1.next;
-            }
-            if (l2) {
-                carry += l2.val;
-                l2 = l2.next;
-            }
-            let [div, mod] = [Math.floor(carry / 10), carry % 10];
-            carry = div;
-            cur.next = new ListNode(mod);
-            cur = cur.next;
-        }
-        return res.next;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    const dummy = new ListNode();
+    let carry = 0;
+    let cur = dummy;
+    while (l1 || l2 || carry) {
+        const s = (l1?.val || 0) + (l2?.val || 0) + carry;
+        carry = Math.floor(s / 10);
+        cur.next = new ListNode(s % 10);
+        cur = cur.next;
+        l1 = l1?.next;
+        l2 = l2?.next;
+    }
+    return dummy.next;
 };
