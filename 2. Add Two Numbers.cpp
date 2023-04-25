c7ab13aafa08e1fe6c9e2040a04f86214ce11189
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-04-25 12:55:00 +0600

    Update 2. Add Two Numbers.cpp
    Signed-off-by: MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>

--- 2. Add Two Numbers.cpp
+++ 2. Add Two Numbers.cpp
@@ -1,28 +1,27 @@
-class Solution
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-    {
-        ListNode *cur = new ListNode();
-        ListNode *res = cur;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
         int carry = 0;
-        while (l1 || l2 || carry)
-        {
-            if (l1)
-            {
-                carry += l1->val;
-                l1 = l1->next;
-            }
-            if (l2)
-            {
-                carry += l2->val;
-                l2 = l2->next;
-            }
-            int value = carry % 10;
-            carry /= 10;
-            cur->next = new ListNode(value);
+        ListNode* cur = dummy;
+        while (l1 || l2 || carry) {
+            int s = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = s / 10;
+            cur->next = new ListNode(s % 10);
             cur = cur->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
         }
-        return res->next;
+        return dummy->next;
     }
 };
